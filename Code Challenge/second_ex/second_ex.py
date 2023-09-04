7d5127fd68c7cb5a28534b464e546b18be43aa88
Lucas <lucas.pereira@leega.com.br>
2023-09-04 23:49:03 +0100

    set cleaners limits

--- Code Challenge/second_ex/second_ex.py
+++ Code Challenge/second_ex/second_ex.py
@@ -10,13 +10,13 @@ clean_basket = []
 farmers = {"fst_farmer": [], "snd_farmer": [], "trd_farmer": []}
 cleaners = {"fst_cleaner": [], "snd_cleaner": [], "trd_cleaner": []}
 
-# Semaphore to control farmer access to the tree
+# Semaphore to control farmer access to the tree. Lmits 1 task per time.
 farmer_semaphore = asyncio.Semaphore(1)
 
 # Flag to control execution
 stop_execution = False
 
-
+# Async function to log every second
 async def print_lengths():
     while not stop_execution:
         print(
@@ -25,6 +25,7 @@ async def print_lengths():
         await asyncio.sleep(1)
 
 
+# Async function for the farmers
 async def farmers_task():
     while tree:
         async with farmer_semaphore:
@@ -37,19 +38,26 @@ async def farmers_task():
                 farmer.remove(fruit)
                 dirty_basket.append(fruit)
 
-
+# Async function for the cleaners
 async def cleaners_task():
     while True:
         if len(dirty_basket) > 0:
-            fruit = dirty_basket.pop()
-            #cleaner = cleaners[cleaner_name]
-            cleaner = cleaners[random.choice(list(cleaners.keys()))]
-            cleaner.append(fruit)
-            await asyncio.sleep(random.uniform(2,4))
-            cleaner.remove(fruit)
-            clean_basket.append(fruit)
+            fruit = random.choice(dirty_basket)
+            # Ensure that cleaners are managing the fruit individually
+            available_cleaners = [cleaner for cleaner, cleaned_fruits in cleaners.items() if len(cleaned_fruits) == 0]
+            if available_cleaners:
+                chosen_cleaner = random.choice(available_cleaners)
+                dirty_basket.remove(fruit)
+                cleaners[chosen_cleaner].append(fruit)
+                await asyncio.sleep(random.uniform(2, 4))
+                cleaners[chosen_cleaner].remove(fruit)
+                clean_basket.append(fruit)
+            else:
+                # No available cleaners, wait for one to become available
+                await asyncio.sleep(1)
         else:
             await asyncio.sleep(1)
+
 
 
 async def main():
@@ -62,13 +70,14 @@ async def main():
     # Create tasks for cleaners
     cleaner_tasks = [loop.create_task(cleaners_task()) for _ in range(len(cleaners.keys()))]
 
+    # Stop the execution when all the fruits were collected and cleaned
     global stop_execution
     while True:
         if len(tree) == 0 and len(dirty_basket) == 0 and len(clean_basket) == 50:
             stop_execution = True
             break
         await asyncio.sleep(1)
-
+    #  Avoid terminating the main task prematurely
     for task in asyncio.all_tasks():
         if task != asyncio.current_task():
             print('All the fruits were collected and cleaned.')
